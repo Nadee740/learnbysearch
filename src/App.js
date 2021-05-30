7eb6c8358e374c1fbd284f2e16b1519d79ef45e6
Don Jose Mathew <59858506+dontech09@users.noreply.github.com>
2021-05-30 14:45:14 +0530

    Update Profile

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
 // import logo from './logo.svg';
-import Navbar from './NavBar/Navbar';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import Login from './login/Login';
-import Signup from './Signup/Signup';
-import Examples from './Popup/Popup';
-import LandingPage from './LandingPage/LandingPage';
-import Page1 from './Page1/Page1';
-import { useEffect, useState } from 'react';
-import Session from 'react-session-persist/lib';
-import EditProfile from './EditProfile/EditProfile';
+import Navbar from "./NavBar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./login/Login";
+import Signup from "./Signup/Signup";
+import Examples from "./Popup/Popup";
+import LandingPage from "./LandingPage/LandingPage";
+import Page1 from "./Page1/Page1";
+import { useEffect, useState } from "react";
+import Session from "react-session-persist/lib";
+import EditProfile from "./EditProfile/EditProfile";
 
 function App() {
-  const [user,setUser]=useState()
+  const [user, setUser] = useState();
   useEffect(() => {
-    
-    const loggedInUser = sessionStorage.getItem('user')
-   
+    const loggedInUser = sessionStorage.getItem("user");
+
     if (loggedInUser) {
-      
-setUser(loggedInUser)
-      
+      setUser(loggedInUser);
     }
-      
-   }, [])
-  
+  }, []);
+
   return (
-    
     <div className="App">
-      
-       <Router>
-       <Navbar/>
-       {user ? <Route path="/" exact component={Page1}  /> : <Route path="/" exact component={LandingPage}  /> }
-       <Route path='/login' component={Login} />
-       <Route path='/signup' component={Signup} />
-       <Route path='/editprofile' component={EditProfile} />
-       <Route path='/page1' component={Page1} />
-       <Route path='/verification' component={Examples} />
-       </Router>
-      
+      <Router>
+        <Navbar />
+        {user ? (
+          <Route path="/" exact component={Page1} />
+        ) : (
+          <Route path="/" exact component={LandingPage} />
+        )}
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/editprofile" component={EditProfile} />
+        <Route path="/page1" component={Page1} />
+        <Route path="/verification" component={Examples} />
+      </Router>
     </div>
-   
   );
 }
 
