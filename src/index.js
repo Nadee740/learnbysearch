ee2ec4f2b30bf61e27f5484cd3f8e7023e020f1d
Nadeem740 <nadeemblayparambil@gmail.com>
2021-06-01 17:43:07 +0530

    30

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Session from 'react-session-persist/lib';
-window.name = "http://localhost:8000/api/"
+window.name = "http://13.127.98.11/"
 ReactDOM.render(
   <React.StrictMode>
    <Session>
