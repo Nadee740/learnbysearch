0d54f34aad316da37ecc10374fcee816d717a541
Don Jose Mathew <59858506+dontech09@users.noreply.github.com>
2021-06-01 15:06:57 +0530

    Blog section

--- src/Blogspage/Blogspage.js
+++ src/Blogspage/Blogspage.js
@@ -1,11 +1,36 @@
+import "./Blogspage.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import Blog from "./blog";
 const BlogsPage = () => {
-    return (  
-        <>
-        
-        
-        
-        </>
-    );
-}
- 
-export default BlogsPage
+  return (
+    <div className="blogpage">
+      <div className="blogheader">
+        <div className="blogheader-col1">
+          <img
+            src="/images/change.png"
+            alt="Blog Section"
+            className="blogheader-img"
+          />
+        </div>
+        <div className="blogheader-col2">
+          <h2>Blogs</h2>
+          <p className="blogheader-text">
+            Blogging is a conversation , not a code
+          </p>
+        </div>
+      </div>
+      <div className="searchbarholder">
+        <input type="text" className="searchbar" placeholder="Search Blogs" />
+        <AiOutlineSearch size="2em" className="searchbar-icn" />
+      </div>
+      <div className="blogholder">
+        <Blog />
+        <Blog />
+        <Blog />
+        <Blog />
+      </div>
+    </div>
+  );
+};
+
+export default BlogsPage;
