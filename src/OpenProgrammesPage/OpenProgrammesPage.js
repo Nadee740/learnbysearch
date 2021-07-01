571f309bb1b4c4bf131d241fa07ebedb86e86cd7
Nadeem740 <nadeemblayparambil@gmail.com>
2021-07-01 19:25:16 +0530

    hy

--- src/OpenProgrammesPage/OpenProgrammesPage.js
+++ src/OpenProgrammesPage/OpenProgrammesPage.js
@@ -249,7 +249,7 @@ function OpenProgrammesPage() {
                       Total Duration
                     </p>
                     <p className="vaccency-item-text">
-                      Approximately {blogsData.duration} 
+                      {blogsData.duration} 
                     </p>
                   </div>
                   <div className="openprogrammespage-feature-col openprogrammespage-feature-col2">
