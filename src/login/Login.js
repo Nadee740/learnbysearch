a189e97b299a138d2419f6e1ad7832c7966d19d3
Don Jose Mathew <59858506+dontech09@users.noreply.github.com>
2021-08-16 13:06:12 +0530

    Referral

--- src/login/Login.js
+++ src/login/Login.js
@@ -10,7 +10,7 @@ import { HiEye, HiEyeOff } from "react-icons/hi";
 import Authverifier from "../Backend/Authverifier";
 import RotateCircleLoading from "react-loadingg/lib/RotateCircleLoading";
 import SolarSystemLoading from "react-loadingg/lib/SolarSystemLoading";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
 const Login = () => {
   const [username, setUser] = useState("");
@@ -24,30 +24,29 @@ const Login = () => {
   const [visible, setvisible] = useState(false);
   const [forgotpassvisible, setforgotpassvisible] = useState(false);
   const [resendmailvisible, setresendmailvisible] = useState(false);
-const [isLoggedIn, setisLoggedin] = useState(false);
-  
+  const [isLoggedIn, setisLoggedin] = useState(false);
 
   const [isLoading, setisLoading] = useState(false);
   const stylefunction = (color, id) => {
     document.getElementById(id).style.border = color;
   };
 
-  useEffect(async()=>{
-    setisLoading(true)
+  useEffect(async () => {
+    setisLoading(true);
     const { isLoggedIn: messagee, data: datas } = await Authverifier(
       `${window.name}users/me`
     );
-    setisLoggedin(messagee)
+    setisLoggedin(messagee);
     setisLoading(false);
-  },[])
+  }, []);
 
   const closeModal = () => {
     setvisible(false);
     setresendmailvisible(true);
   };
- const closeModalmailnot=()=>{
-  setvisible(false);
- }
+  const closeModalmailnot = () => {
+    setvisible(false);
+  };
 
   const closeforgotpassModal = () => {
     setforgotpassvisible(false);
@@ -64,7 +63,7 @@ const [isLoggedIn, setisLoggedin] = useState(false);
       `${window.name}resend-email-verification`,
       log_data
     );
-  
+
     setemailerr(messagee);
   };
 
@@ -82,8 +81,8 @@ const [isLoggedIn, setisLoggedin] = useState(false);
 
   const output = async (e) => {
     e.preventDefault();
-  myTrim(username)
-    setisLoading(true)
+    myTrim(username);
+    setisLoading(true);
     // stylefunction("2px solid #81818128", "passholder");
     // stylefunction("2px solid #81818128", "usernameholder");
     setpassrerror();
@@ -98,33 +97,29 @@ const [isLoggedIn, setisLoggedin] = useState(false);
     if (messagee.includes("your account is not verifed")) {
       setisLoading(false);
       setvisible(true);
-    }
-    else if (!messagee.includes("successfully")) {
+    } else if (!messagee.includes("successfully")) {
       // stylefunction("2px outset red", "passholder");
       // stylefunction("2px outset red", "usernameholder");
       setpassrerror(messagee);
       setisLoading(false);
     } else {
-      
       window.location = "/openprogrammes";
       setUser("");
       setPass("");
     }
-  
   };
 
   function myTrim(x) {
-  console.log(x.replace(/^\s+|\s+$/gm,''));
-}
+    console.log(x.replace(/^\s+|\s+$/gm, ""));
+  }
 
   return (
     <>
-     <Helmet>
-                <meta charSet="utf-8" />
-                <title>Home</title>
-                
-            </Helmet>
-   
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Home</title>
+      </Helmet>
+
       <div className="popupscreen">
         <section
           className="popupscr
@@ -143,7 +138,9 @@ const [isLoggedIn, setisLoggedin] = useState(false);
                 className="logo"
                 alt=""
               />
-             <p>Verification email already sent on <br /> email address.</p>
+              <p>
+                Verification email already sent on <br /> email address.
+              </p>
               <br />
               <Link onClick={closeModal}>Resend verification mail</Link>
             </div>
@@ -221,97 +218,124 @@ const [isLoggedIn, setisLoggedin] = useState(false);
         </section>
       </div>
 
-      {isLoading?(
+      {isLoading ? (
         <div className="isLoading">
-        <SolarSystemLoading/>
+          <SolarSystemLoading />
         </div>
-      ):isLoggedIn?(
+      ) : isLoggedIn ? (
         <div className="isLoading">
           <h1>Already Logged In</h1>
         </div>
-      ):
-      <section className="sign-in">
-        <div className="container">
-          <div className="signup-content">
-            <div className="signup-image">
-              <img src="../images/SignIn.svg" alt="Login pic"></img>
-            </div>
-            <div className="singup-form">
-              <h2 className="form-title">WELCOME BACK!</h2>
-              <form onSubmit={output}>
-                <div className="inputholder" id="usernameholder">
-                  <div className="inputholder-top">
-                    <input
-                    required
-                      type="text"
-                      placeholder="Username"
-                      id="username"
-                      onChange={(e) => {
-                        setUser(e.target.value);
-                      }}
-                    />
-                  </div>
-                </div>
-
-                <div className="inputholder" id="passholder">
-                  <div className="inputholder-top">
-                    <input
-                    required
-                      id="password"
-                      type={passVISIBLE ? "text" : "password"}
-                      placeholder="Password"
-                      onChange={(e) => {
-                        setPass(e.target.value);
-                      }}
-                    />
-                    {passVISIBLE ? (
-                      <HiEye
-                        size="1.3em"
-                        color="#404040"
-                        onClick={() => {
-                          setpassVISIBLE(!passVISIBLE);
+      ) : (
+        <section className="sign-in">
+          <div className="container">
+            <div className="signup-content">
+              <div className="signup-image">
+                <img src="../images/SignIn.svg" alt="Login pic"></img>
+              </div>
+              <div className="singup-form">
+                <h2 className="form-title">WELCOME BACK!</h2>
+                <form onSubmit={output}>
+                  <div className="inputholder" id="usernameholder">
+                    <div className="inputholder-top">
+                      <input
+                        required
+                        type="text"
+                        placeholder="Username"
+                        id="username"
+                        onChange={(e) => {
+                          setUser(e.target.value);
                         }}
                       />
-                    ) : (
-                      <HiEyeOff
-                        size="1.3em"
-                        color="#404040"
-                        onClick={() => {
-                          setpassVISIBLE(!passVISIBLE);
+                    </div>
+                  </div>
+
+                  <div className="inputholder" id="passholder">
+                    <div className="inputholder-top">
+                      <input
+                        required
+                        id="password"
+                        type={passVISIBLE ? "text" : "password"}
+                        placeholder="Password"
+                        onChange={(e) => {
+                          setPass(e.target.value);
                         }}
                       />
-                    )}
+                      {passVISIBLE ? (
+                        <HiEye
+                          size="1.3em"
+                          color="#404040"
+                          onClick={() => {
+                            setpassVISIBLE(!passVISIBLE);
+                          }}
+                        />
+                      ) : (
+                        <HiEyeOff
+                          size="1.3em"
+                          color="#404040"
+                          onClick={() => {
+                            setpassVISIBLE(!passVISIBLE);
+                          }}
+                        />
+                      )}
+                    </div>
+                    <label className="label" htmlFor="">
+                      {passerror && passerror}
+                    </label>
                   </div>
-                  <label className="label" htmlFor="">
-                    {passerror && passerror}
-                  </label>
-                </div>
 
-                <input
-                  type="submit"
-                  value="SIGN IN"
-                  placeholder="Sign Up"
-                  className="submit-btn"
-                />
-                <Link to="/signup">
-                  <p className="form-btmtext form-btmtext1">
-                    Not registered ? Signup
-                  </p>
-                </Link>
+                  <div
+                    className="referal"
+                    style={{
+                      width: "100%",
+                    }}
+                  >
+                    <p className="reffereltext">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Totam ipsa possimus quas molestiae, rerum reprehenderit
+                      eos, fugiat ea animi velit aspernatur facilis non corporis
+                      ut, natus doloremque sapiente quis harum?
+                    </p>
+                    <div className="inputholder" id="passholder">
+                      <div className="inputholder-top">
+                        <input
+                          required
+                          id="password"
+                          type="text"
+                          placeholder="Refferel Code"
+                        />
+                      </div>
+                      <label className="label" htmlFor="">
+                        Error Text
+                      </label>
+                    </div>
+                    <button className="referal-btn">GENERATE</button>
+                  </div>
+                  <input
+                    type="submit"
+                    value="SIGN IN"
+                    placeholder="Sign Up"
+                    className="submit-btn"
+                  />
+                  <Link to="/signup">
+                    <p className="form-btmtext form-btmtext1">
+                      Not registered ? Signup
+                    </p>
+                  </Link>
 
-                <Link
-                  onClick={() => {
-                    setforgotpassvisible(true);
-                  }}
-                >
-                  <p className="form-btmtext">Forgot Password</p>
-                </Link>
-              </form>
+                  <Link
+                    onClick={() => {
+                      setforgotpassvisible(true);
+                    }}
+                  >
+                    <p className="form-btmtext">Forgot Password</p>
+                  </Link>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      }
+        </section>
+      )}
     </>
   );
 };
