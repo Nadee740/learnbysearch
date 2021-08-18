54d24b06cb5713dda539ae5fd20a87b46a95a08f
Nadeem740 <nadeemblayparambil@gmail.com>
2021-08-18 16:03:37 +0530

    webinar reg

--- src/webinar/webinarcard.js
+++ src/webinar/webinarcard.js
@@ -1,9 +1,118 @@
 import React from "react";
 import "./webinar.css";
 import { MdDateRange, MdVideoCall } from "react-icons/md";
+import { IoArrowForwardOutline } from "react-icons/io5";
+import { useState } from "react";
+import Authverifier from "../Backend/Authverifier";
+import SendPost from "../Backend/Sendpost";
+import { Link } from "react-router-dom";
+import Modal from "react-awesome-modal";
 const WebinareCard = ({webinardata}) => {
-  console.log(webinardata)
+  const [visible, setvisible] = useState(false);
+  const [loginvisible, setloginvisible] = useState(false);
+  const [isloggedin, setisLoggedin] = useState(false);
+
+  const applyforwebinar = async () => {
+    const { isLoggedIn: isLoggedIn } = await Authverifier(`${window.name}users/me`
+    );
+    if (isLoggedIn) {
+      const data = {
+        webinarId: webinardata._id,
+      };
+      const { message: outcome } = await SendPost(
+        `${window.name}apply-for-webinar`,
+        data
+      );
+
+      if (outcome.includes("success")) {
+        closeModal();
+        alert(`Successfully registered for\n${webinardata.title}`);
+      } else {
+        closeModal();
+        alert("Sorry an error occured");
+      }
+    } else {
+      setvisible(false);
+      setloginvisible(true);
+    }
+  };
+
+  const closeModal = () => {
+    setvisible(false);
+  };
+
+  const closeloginModal = () => {
+    setloginvisible(false);
+  };
+
   return (
+    <>
+      <div className="popupscreen">
+        <section className="popupscreen">
+          <Modal
+            visible={visible}
+            width="350"
+            height="200"
+            effect="fadeInUp"
+            onClickAway={closeModal}
+          >
+            <div className="popup">
+              <img
+                src="/images/LearnByResearchLogo.png"
+                className="logo"
+                alt=""
+              />
+              <p>Are you sure you want to apply for {webinardata.title}?</p>
+              <br></br>
+              <div className="extrapart-webinar">
+                <div className="signuppart">
+                  <Link onClick={closeModal}>Close</Link>
+                </div>
+                <div>
+                  <Link onClick={applyforwebinar}>Apply</Link>
+                </div>
+              </div>
+            </div>
+          </Modal>
+        </section>
+      </div>
+      <div className="popupscreen">
+        <section className="popupscreen">
+          <Modal
+            visible={loginvisible}
+            width="350"
+            height="200"
+            effect="fadeInUp"
+            onClickAway={closeloginModal}
+          >
+            <div className="popup">
+              <img
+                src="/images/LearnByResearchLogo.png"
+                className="logo"
+                alt=""
+              />
+              <p>PLEASE LOGIN TO SUBMIT APPLICATION...</p>
+              <br></br>
+              <div className="extrapart">
+                <div className="signuppart">
+                  <Link to="/signup" onClick={closeloginModal}>
+                    Sign Up
+                  </Link>
+                </div>
+                <div>
+                  <Link onClick={closeloginModal}>Close</Link>
+                </div>
+
+                <div className="loginpart">
+                  <Link to="/login" onClick={closeloginModal}>
+                    Login
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </Modal>
+        </section>
+      </div>
     <div className="webinarcard">
       <img
         src={webinardata.imageUrl}
@@ -17,14 +126,17 @@ const WebinareCard = ({webinardata}) => {
       <p className="webinarcard-head">
        {webinardata.title}
       </p>
-      {/* <p className="webinarcard-person">
-        Speaker: <span>Rishi Kapal</span>
-      </p>
-      <p className="webinarcard-date webinarcard-link">
-        <MdVideoCall size="1.6em" />
-        https://meet.google.com/tqp-gzgg-noj
-      </p> */}
+      <button
+            className="regbtn"
+            onClick={() => {
+               setvisible(true);
+            }}
+          >
+            Register Now
+            <IoArrowForwardOutline className="webicn" />
+          </button>
     </div>
+    </>
   );
 };
 
