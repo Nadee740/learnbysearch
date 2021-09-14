0a6392905d07e7fd7d1322bc7cae1d5542a98ce6
dontech09 <donnjondanmakal@gmail.com>
2021-09-14 15:33:26 +0530

    Payments

--- src/PaymentStatus/PaymentStatusPage.js
+++ src/PaymentStatus/PaymentStatusPage.js
@@ -23,8 +23,54 @@ const PaymentStatusPage = () => {
       <div className="application-container">
         <div className="applications-heading">
           <h2>My Payments</h2>
+        </div>{" "}
+        <div className="myapplication-box">
+          <div className="myapplication-row">
+            <div className="paytable-head ">
+              Research Programme:
+              <span>Prototyping the future of retail management</span>
+            </div>
+          </div>
+          <div className="myapplication-box-holder">
+            <div className="myapplication-row myapplication-row-head">
+              <p className="myapplication-row-text myapplication-row-text-head ">
+                Due Date
+              </p>
+              <p className="myapplication-row-text myapplication-row-text-head ">
+                Status
+              </p>
+              <p className="myapplication-row-text myapplication-row-text-head "></p>
+            </div>
+            <div className="myapplication-row">
+              <p className="myapplication-row-text ">29/09/2021</p>
+              <p className="myapplication-row-text ">Paid</p>
+              <p className="myapplication-row-text  ">
+                <button className="pay-btn pay-btn-paid">Paid</button>
+              </p>
+            </div>
+            <div className="myapplication-row">
+              <p className="myapplication-row-text ">29/09/2021</p>
+              <p className="myapplication-row-text ">Not Paid</p>
+              <p className="myapplication-row-text  ">
+                <button className="pay-btn pay-btn-np">Pay Now</button>
+              </p>
+            </div>
+            <div className="myapplication-row">
+              <p className="myapplication-row-text ">29/09/2021</p>
+              <p className="myapplication-row-text ">Not Paid</p>
+              <p className="myapplication-row-text  ">
+                <button className="pay-btn pay-btn-deactivated">Pay Now</button>
+              </p>
+            </div>
+          </div>
         </div>
         <div className="myapplication-box">
+          <div className="myapplication-row">
+            <div className="paytable-head ">
+              Research Programme:
+              <span>Modeling and Analysis of Biomimetic Drone</span>
+            </div>
+          </div>
           <div className="myapplication-box-holder">
             <div className="myapplication-row myapplication-row-head">
               <p className="myapplication-row-text myapplication-row-text-head ">
