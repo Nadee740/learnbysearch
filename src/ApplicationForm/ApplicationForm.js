163df1412bc858f28ed66d1ebb1e7ddadb289b9f
dontech09 <donnjondanmakal@gmail.com>
2021-09-22 16:30:45 +0530

    Application Form

--- src/ApplicationForm/ApplicationForm.js
+++ src/ApplicationForm/ApplicationForm.js
@@ -10,13 +10,13 @@ import { RotateCircleLoading } from "react-loadingg";
 import SolarSystemLoading from "react-loadingg/lib/SolarSystemLoading";
 import { Helmet } from "react-helmet";
 import UserReferalCode from "../Backend/usereferalcode";
-
+import { Stepper, Step } from "react-form-stepper";
 const ApplicationForm = () => {
   const [visible, setvisible] = useState(false);
   const [referalvisible, setreferalvisible] = useState(false);
   const [paravisible, setparavisible] = useState(false);
   const [errorvisible, seterrorvisible] = useState(false);
-
+  const [step, setStep] = useState(0); //Stepper
   const { slug } = useParams();
   const [isLoading, setisLoading] = useState(true);
   const [q1, setq1] = useState("");
@@ -257,162 +257,219 @@ const ApplicationForm = () => {
                 </div>
                 <div className="singup-form">
                   <h2 className="form-title">APPLICATION </h2>
-                  <form onSubmit={submitApplictaionform}>
-                    <p className="inputtext">Referal Code if any</p>
-                    <div
-                      className="inputholder inputholder2"
-                      id="usernameholder"
-                    >
-                      <div className="inputholder-top ">
-                        <textarea
-                          minLength={100}
-                          rows="1"
-                          className="textarea"
-                          placeholder=""
-                          value={code}
-                          onChange={(e) => {
-                            setreferalcode(e.target.value);
-                          }}
-                        ></textarea>
-                      </div>
-                    </div>
-                    <p className="inputtext">
-                      What do you want to achieve by joining the research
-                      program?
-                    </p>
-                    <div
-                      className="inputholder inputholder2"
-                      id="usernameholder"
-                    >
-                      <div className="inputholder-top ">
-                        <textarea
-                          minLength={100}
-                          rows="5"
-                          className="textarea"
-                          placeholder=""
-                          required
-                          value={q1}
-                          onChange={(e) => {
-                            setq1(e.target.value);
-                          }}
-                        ></textarea>
-                      </div>
-                    </div>
-                    <p className="inputtext">
-                      Select the position for which you are applying :
-                    </p>
-                    <div
-                      className="inputholder inputholder2"
-                      id="usernameholder"
-                    >
-                      <div className="inputholder-top inputholder-top3">
-                        {/* <p className="inputp">
-                          SELECT THE POSITION FOR WHICH YOU HAVE TO APPLY !
-                        </p> */}
-                        <div className="div">
-                          <select
-                            className="selectbx"
-                            onChange={(e) => {
-                              setPositionId(e.target.value);
-                            }}
-                          >
-                            {isLoading
-                              ? setisLoading(true)
-                              : positions &&
-                                positions.map((position, index) => (
-                                  <option
-                                    value={position._id}
-                                    className="selectbx-itm"
-                                    key={index}
-                                  >
-                                    {position.title}
-                                  </option>
-                                ))}
-                          </select>
-                        </div>
-                      </div>
-                    </div>
-                    <p className="inputtext">
-                      Can you afford to pay the program fees of Rs 3000 per
-                      month?
-                      <br /> (Duration 3-4 months as it varies with the research
-                      project and is mentioned on the website. This fees will
-                      cover the charges for mentorship, training resources,
-                      publication charges and the tools required.)
-                    </p>
-                    <div
-                      className="inputholder inputholder2"
-                      id="usernameholder"
-                    >
-                      <div className="inputholder-top inputholder-top3  ">
-                        {/* <p className="inputp">
-                          The Program charges 5000 fees to cover the
-                          research,training and resources cost.Can you afford to
-                          pay the fees?( If eligible you may get financial
-                          assistance to support. )
-                        </p> */}
-                        <div className="div ">
-                          <select
-                            className="selectbx"
-                            onChange={(e) => {
-                              setq2(e.target.value);
-                              if (e.target.value == "true") {
-                                setparavisible(false);
-                              } else {
-                                setparavisible(true);
-                              }
-                            }}
-                          >
-                            <option value={true} className="selectbx-itm">
-                              YES
-                            </option>
-                            <option value={false} className="selectbx-itm">
-                              NO
-                            </option>
-                          </select>
-                        </div>
-                      </div>
-                    </div>
 
-                    {paravisible ? (
+                  <form onSubmit={submitApplictaionform}>
+                    <div className="stepperdiv">
+                      <Stepper
+                        activeStep={step}
+                        styleConfig={{
+                          completedBgColor: "#bd00c0",
+                          activeBgColor: "#C86FC9",
+                          inactiveBgColor: "#bdbdbd",
+                        }}
+                      >
+                        <Step label="Application Form" />
+
+                        <Step label="Financial Assistance" />
+                      </Stepper>
+                    </div>
+                    {step === 0 ? (
                       <>
-                        <p className="inputtext">
-                          If You Want financial assistance please mention your
-                          annual family income and tell us how you can help
-                          LearnByResearch to support others in need of
-                          assistance like you :
-                        </p>
+                        <p className="inputtext">Referal Code if any</p>
                         <div
-                          className="inputholder inputholder2 "
+                          className="inputholder inputholder2"
                           id="usernameholder"
                         >
                           <div className="inputholder-top ">
                             <textarea
-                              minLength={250}
-                              value={q3}
-                              onChange={(e) => {
-                                setq3(e.target.value);
-                              }}
-                              rows="15"
+                              minLength={100}
+                              rows="1"
                               className="textarea"
                               placeholder=""
+                              value={code}
+                              onChange={(e) => {
+                                setreferalcode(e.target.value);
+                              }}
                             ></textarea>
                           </div>
-                          <label className="label" htmlFor="">
-                            {err && err}
-                          </label>
+                        </div>
+                        <p className="inputtext">
+                          What do you want to achieve by joining the research
+                          program?
+                        </p>
+                        <div
+                          className="inputholder inputholder2"
+                          id="usernameholder"
+                        >
+                          <div className="inputholder-top ">
+                            <textarea
+                              minLength={100}
+                              rows="5"
+                              className="textarea"
+                              placeholder=""
+                              required
+                              value={q1}
+                              onChange={(e) => {
+                                setq1(e.target.value);
+                              }}
+                            ></textarea>
+                          </div>
+                        </div>
+                        <p className="inputtext">
+                          Select the position for which you are applying :
+                        </p>
+                        <div
+                          className="inputholder inputholder2"
+                          id="usernameholder"
+                        >
+                          <div className="inputholder-top inputholder-top3">
+                            {/* <p className="inputp">
+                          SELECT THE POSITION FOR WHICH YOU HAVE TO APPLY !
+                        </p> */}
+                            <div className="div">
+                              <select
+                                className="selectbx"
+                                onChange={(e) => {
+                                  setPositionId(e.target.value);
+                                }}
+                              >
+                                {isLoading
+                                  ? setisLoading(true)
+                                  : positions &&
+                                    positions.map((position, index) => (
+                                      <option
+                                        value={position._id}
+                                        className="selectbx-itm"
+                                        key={index}
+                                      >
+                                        {position.title}
+                                      </option>
+                                    ))}
+                              </select>
+                            </div>
+                          </div>
                         </div>
                       </>
                     ) : (
                       ""
                     )}
+                    {step === 1 ? (
+                      <>
+                        <p className="inputtext">
+                          Can you afford to pay the program fees of Rs 3000 per
+                          month?
+                          <br /> (Duration 3-4 months as it varies with the
+                          research project and is mentioned on the website. This
+                          fees will cover the charges for mentorship, training
+                          resources, publication charges and the tools
+                          required.)
+                        </p>
+                        <div
+                          className="inputholder inputholder2"
+                          id="usernameholder"
+                        >
+                          <div className="inputholder-top inputholder-top3  ">
+                            {/* <p className="inputp">
+                          The Program charges 5000 fees to cover the
+                          research,training and resources cost.Can you afford to
+                          pay the fees?( If eligible you may get financial
+                          assistance to support. )
+                        </p> */}
+                            <div className="div ">
+                              <select
+                                className="selectbx"
+                                onChange={(e) => {
+                                  setq2(e.target.value);
+                                  if (e.target.value == "true") {
+                                    setparavisible(false);
+                                  } else {
+                                    setparavisible(true);
+                                  }
+                                }}
+                              >
+                                <option value={true} className="selectbx-itm">
+                                  YES
+                                </option>
+                                <option value={false} className="selectbx-itm">
+                                  NO
+                                </option>
+                              </select>
+                            </div>
+                          </div>
+                        </div>
 
-                    <input
+                        {paravisible ? (
+                          <>
+                            <p className="inputtext">
+                              If You Want financial assistance please mention
+                              your annual family income and tell us how you can
+                              help LearnByResearch to support others in need of
+                              assistance like you :
+                            </p>
+                            <div
+                              className="inputholder inputholder2 "
+                              id="usernameholder"
+                            >
+                              <div className="inputholder-top ">
+                                <textarea
+                                  minLength={250}
+                                  value={q3}
+                                  onChange={(e) => {
+                                    setq3(e.target.value);
+                                  }}
+                                  rows="15"
+                                  className="textarea"
+                                  placeholder=""
+                                ></textarea>
+                              </div>
+                              <label className="label" htmlFor="">
+                                {err && err}
+                              </label>
+                            </div>
+                          </>
+                        ) : (
+                          ""
+                        )}
+                      </>
+                    ) : (
+                      ""
+                    )}
+                    {/*<input
                       type="submit"
                       value="Submit"
                       placeholder="Sign Up"
                       className="submit-btn"
-                    />
+                    />*/}
+                    <div className="btnholder">
+                      {step === 0 ? (
+                        <button
+                          className="submit-btn submit-btn2"
+                          onClick={() => {
+                            setStep(step + 1);
+                          }}
+                        >
+                          NEXT STEP
+                        </button>
+                      ) : null}
+                      {step > 0 ? (
+                        <button
+                          className="submit-btn submit-btn2"
+                          onClick={() => {
+                            setStep(step - 1);
+                          }}
+                        >
+                          PREVIOUS STEP
+                        </button>
+                      ) : null}
+                      {step > 0 ? (
+                        <button
+                          className="submit-btn submit-btn2"
+                          onClick={() => {}}
+                        >
+                          SUBMIT
+                        </button>
+                      ) : null}
+                    </div>
                   </form>
                 </div>
               </div>
