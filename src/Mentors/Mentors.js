006ecd04c9cc64f1ec6ffc475671e1b12b9b3950
dontech09 <donnjondanmakal@gmail.com>
2021-12-22 11:27:06 +0530

    Data reload after adding

--- src/Mentors/Mentors.js
+++ src/Mentors/Mentors.js
@@ -6,45 +6,39 @@ import SolarSystemLoading from "react-loadingg/lib/SolarSystemLoading";
 const Mentors = () => {
   const [mentors, setmentors] = useState("");
   const [isLoading, setisLoading] = useState(true);
-  useEffect(async()=>{
+  useEffect(async () => {
     setisLoading(true);
     const { data: mentors } = await Researchpgms(`${window.name}mentors`);
     setmentors(mentors);
     setisLoading(false);
-  },[])
-  if(isLoading){
-  return(
-    <div className="isLoading">
-      <SolarSystemLoading />
-    </div>
-  ) 
-
+  }, []);
+  if (isLoading) {
+    return (
+      <div className="isLoading">
+        <SolarSystemLoading />
+      </div>
+    );
   }
   return (
     <>
       <div className="mentorssec">
-        <h2 className="mentorcarousel-head">Our Mentors</h2>
-        {
-          mentors.map((mentor,index)=>{
-    
-let position = mentor.position.replace(/<\/?[^>]+(>|$)/g, "");
-            return(
-              <div className="mentor-grid">
-          <div className="mentorcarousel-card-inner">
-            <img
-              src={mentor.imageUrl}
-              alt="Mentor image"
-              className="mentorcarousel-img"
-            />
-            <p className="mentorcarousel-card-head">{mentor.name}</p>
-            <p className="mentorcarousel-card-sub">{position}</p>
-          </div>
-         </div>
-            )
-          })
-        }
-        
-     
+        <h2 className="mentorcarousel-head">Our Mentors</h2>{" "}
+        <div className="mentor-grid">
+          {mentors.map((mentor, index) => {
+            let position = mentor.position.replace(/<\/?[^>]+(>|$)/g, "");
+            return (
+              <div className="mentorcarousel-card-inner">
+                <img
+                  src={mentor.imageUrl}
+                  alt="Mentor image"
+                  className="mentorcarousel-img"
+                />
+                <p className="mentorcarousel-card-head">{mentor.name}</p>
+                <p className="mentorcarousel-card-sub">{position}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <Footer />
     </>
