8b39a4edbc6dbf6a5ad61a8bed3aedf2adabb8bf
Nadeem740 <nadeemblayparambil@gmail.com>
2021-12-26 18:40:30 +0530

    quiz

--- src/quiz/quiz.js
+++ src/quiz/quiz.js
@@ -9,6 +9,7 @@ const QuizSection = () => {
   const { positionid } = useParams();
   const [quizdata,setquizdata]=useState();
   const [isLoading,setisLoading]=useState(true);
+  const [score,setscore]=useState(0);
 
   useEffect(async()=>{
 
@@ -35,21 +36,78 @@ const QuizSection = () => {
               <p className="quiz-question">
                 {index+1 +".  " + question.question}
               </p>
-              <button className="quiz-option">
+              <button className="quiz-option" onClick={()=>{
+                if(question.correctOption==1)
+                   {setscore(score+1)
+                   console.log(score)
+                   }
+                else
+                {if(score>0)
+                  {
+                    setscore(score-1)
+                  }
+
+
+                }
+              }}>
                 <div className="quiz-option-circle">A</div> {question.option1}
               </button>
-              <button className="quiz-option">
+              <button className="quiz-option" onClick={()=>{
+                if(question.correctOption==2)
+                   {setscore(score+1)
+                   
+                   }
+                   else
+                {if(score>0)
+                  {
+                    setscore(score-1)
+                  }
+
+
+                }
+              }}>
                 <div className="quiz-option-circle">B</div> {question.option2}
               </button>
-              <button className="quiz-option">
+              <button className="quiz-option" onClick={()=>{
+                if(question.correctOption==3)
+                   {setscore(score+1)
+                   console.log(score)
+                   }
+                   else
+                {if(score>0)
+                  {
+                    setscore(score-1)
+                  }
+
+
+                }
+              }}>
                 <div className="quiz-option-circle">C</div>{question.option3}
+              </button>
+              <button className="quiz-option" onClick={()=>{
+                if(question.correctOption==4)
+                   {setscore(score+1)
+                   console.log(score)
+                   }
+                   else
+                {if(score>0)
+                  {
+                    setscore(score-1)
+                  }
+
+
+                }
+              }}>
+                <div className="quiz-option-circle">D</div>{question.option4}
               </button>
             </div>
           );
         })}
 
         <div className="quiz-footer">
-          <button className="quiz-footer-btn">Submit</button>
+          <button onClick={()=>{
+            console.log(score)
+          }} className="quiz-footer-btn">Submit</button>
         </div>
       </div>
       <Footer />
