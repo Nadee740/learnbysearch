9b21340452da7fd091c1923222334ce4951acdaa
Nadeem740 <nadeemblayparambil@gmail.com>
2021-12-28 12:29:15 +0530

    quiz

--- src/quiz/quizconfirmation.js
+++ src/quiz/quizconfirmation.js
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
+import SolarSystemLoading from "react-loadingg/lib/SolarSystemLoading";
+import { useParams } from "react-router-dom";
+import Researchpgms from "../Backend/Researchpgms";
 import Footer from "../LandingPage/footer/footer";
 
 const Quizconfirmtaion = () => {
+    const { rpid } = useParams();
+  const { positionid } = useParams();
+  const {appid}=useParams();
+  const[isLoading,setisLoading]=useState(true)
+  const[rp,setrp]=useState()
+  const[postion,setPosition]=useState()
+  useEffect(async()=>{
+
+    const { data } = await Researchpgms(`${window.name}research-program-id/${rpid}`)
+  setrp(data.title)
+  const { data:pos } = await Researchpgms(`${window.name}position/${positionid}`)
+  setPosition(pos.title)
+  setisLoading(false)
+  },[])
+
+  if (isLoading)
+  return (
+    <div className="isLoading">
+      <SolarSystemLoading/>
+    </div>
+  );
     return ( 
         <>
         <p>
         You are about to take a quiz test for the Research
-Program: rp for the Position  which is one
+Program: {rp} for {postion}  which is one
 of the most important screening criteria at LBR.
         </p>
         <br />
