a6f511d674a5b44331a09a24424e3332c806455f
dontech09 <donnjondanmakal@gmail.com>
2021-12-28 10:35:50 +0530

    Changes to quiz confirmation

--- src/quiz/quizconfirmation.js
+++ src/quiz/quizconfirmation.js
@@ -1,24 +1,39 @@
 import Footer from "../LandingPage/footer/footer";
 
 const Quizconfirmtaion = () => {
-    return ( 
-        <>
-        <p>
-        You are about to take a quiz test for the Research
-Program: rp for the Position  which is one
-of the most important screening criteria at LBR.
+  return (
+    <>
+      <div className="quiz-confirmation">
+        <h2 className="quiz-confirmation-head">Take Your Quiz!</h2>
+        <img
+          src="/images/quizconfirm.svg"
+          alt="Quiz confirm"
+          className="quiz-confirm-img"
+        />
+        <p className="quiz-confirm-txt1">
+          You are about to take a quiz test for the Research Program: rp for the
+          Position which is one of the most important screening criteria at LBR.
+        </p>{" "}
+        <p className="quiz-confirm-txt2">
+          {" "}
+          Time required to take this test is <u>20 minutes</u> . submitting
+          application is the first step. Inorder to be eligible for admission to
+          the research Program, it is mandatory to comlplete this test in the
+          next <u>2 working days</u>.
         </p>
-        <br />
-        <p>   Time required to take this test is 20 minutes.
-submitting application is the first step. Inorder to
-be eligible for admission to the research Program,
-it is mandatory to comlplete this test in the next 2
+        <div className="quiz-confirm-btm">
+          <button className="quiz-confirm-btm-btn1 quiz-confirm-btm-btn">
+            Skip
+          </button>
+          <button className="quiz-confirm-btm-btn2 quiz-confirm-btm-btn">
+            Start Now
+          </button>
+        </div>
+      </div>
 
-working days.
-</p>
-            <Footer></Footer>
-        </>
-     );
-}
- 
+      <Footer></Footer>
+    </>
+  );
+};
+
 export default Quizconfirmtaion;
