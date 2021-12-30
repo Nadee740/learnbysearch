61695c889d3e7ae7ae75a8534e0bdb0eaaa1d12a
dontech09 <donnjondanmakal@gmail.com>
2021-12-30 14:31:25 +0530

    Quiz status Column

--- src/MyApplications/MyApplications.js
+++ src/MyApplications/MyApplications.js
@@ -38,7 +38,6 @@ const MyApplications = () => {
       setisLoading(false);
     } else {
       userdata.applicationForm.map(async (application) => {
-        
         const app_data = {
           id: application,
         };
@@ -48,14 +47,12 @@ const MyApplications = () => {
         );
         count++;
 
-        if (retdata.status!='error') {
-          
-          array.push(retdata);}
+        if (retdata.status != "error") {
+          array.push(retdata);
+        }
 
-        
         if (count == userdata.applicationForm.length) {
           if (array.length > 0) {
-           
             setapplication(array);
 
             getreaserch(array);
@@ -69,163 +66,177 @@ const MyApplications = () => {
   };
 
   const getreaserch = (datas) => {
-   
     datas.map(async (data) => {
       const { data: Datass } = await Researchpgms(
         `${window.name}research-program-id/${data.rp}`
       );
-      
+
       rparray.push(Datass);
       checkcount++;
-      test1.push({rpdata:Datass,applicationstatus:data})
-      
+      test1.push({ rpdata: Datass, applicationstatus: data });
+
       if (datas.length == rparray.length) {
         setrp(rparray);
-        test1.sort(function(a,b){
-          return Date.parse(b.applicationstatus.date.substring(0, 10)) - Date.parse(a.applicationstatus.date.substring(0, 10));
-        })
-        setapplicationsdata(test1)
+        test1.sort(function (a, b) {
+          return (
+            Date.parse(b.applicationstatus.date.substring(0, 10)) -
+            Date.parse(a.applicationstatus.date.substring(0, 10))
+          );
+        });
+        setapplicationsdata(test1);
         setisLoading(false);
-        
       }
     });
   };
-if(isLoading){
-  return(
-  <div className="isLoading">
-          <SolarSystemLoading />
-        </div>)
-}
-if(!isLoggedIn){
-  return( <>
-          <div className="isLoading">
-            <h1>Please Log in</h1>
-            <div className="flex-btn">
-    
-              <Link to="/login" className="button1 btn2">Sign in</Link>
-            </div>
+  if (isLoading) {
+    return (
+      <div className="isLoading">
+        <SolarSystemLoading />
+      </div>
+    );
+  }
+  if (!isLoggedIn) {
+    return (
+      <>
+        <div className="isLoading">
+          <h1>Please Log in</h1>
+          <div className="flex-btn">
+            <Link to="/login" className="button1 btn2">
+              Sign in
+            </Link>
           </div>
-        </>)
-}
-if(isempty){
-  return(
-          <div className="oopps">
-            <h1>Ooops nothing to show ...</h1>
-          </div>
-        )} 
+        </div>
+      </>
+    );
+  }
+  if (isempty) {
+    return (
+      <div className="oopps">
+        <h1>Ooops nothing to show ...</h1>
+      </div>
+    );
+  }
   return (
     <>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Home | Applications</title>
       </Helmet>
-    
-          <div className="application-container">
-            <div className="applications-heading">
-              <h2>My Applications</h2>
+      <div className="application-container">
+        <div className="applications-heading">
+          <h2>My Applications</h2>
+        </div>
+        <div className="myapplication-box">
+          <div className="myapplication-box-holder">
+            <div className="myapplication-row myapplication-row-head">
+              <p className="myapplication-row-text myapplication-row-text-head ">
+                Program
+              </p>
+              <p className="myapplication-row-text myapplication-row-text-head ">
+                Application Date
+              </p>
+              <p className="myapplication-row-text myapplication-row-text-head ">
+                Quiz Status
+              </p>
+              <p className="myapplication-row-text myapplication-row-text-head ">
+                Status
+              </p>
             </div>
-            <div className="myapplication-box">
-              <div className="myapplication-box-holder">
-                <div className="myapplication-row myapplication-row-head">
-                  <p className="myapplication-row-text myapplication-row-text-head ">
-                    Program
+            {applicationsData.map((application, index) => {
+              let classn = "",
+                title = "",
+                classtype = "myapplication-row-btn ";
+
+              switch (application.applicationstatus.data) {
+                case 7: {
+                  classn = "myapplication-row-btn-sub";
+                  title = "Submited";
+                  break;
+                }
+                case 6: {
+                  classn = "myapplication-row-btn-accept";
+                  title = "Archived";
+                  break;
+                }
+                case 5: {
+                  classn = "myapplication-row-btn-arch";
+                  title = "Accepted";
+                  break;
+                }
+                case 4: {
+                  classn = "myapplication-row-btn-approv";
+                  title = "Approved";
+                  break;
+                }
+                case 3: {
+                  classn = "myapplication-row-btn-rej";
+                  title = "Rejected";
+                  break;
+                }
+                case 2: {
+                  classn = "myapplication-row-btn-pend";
+                  title = "Under Review";
+                  break;
+                }
+                case 1: {
+                  classn = "myapplication-row-btn-sub";
+                  title = "Attend Quiz";
+                  break;
+                }
+              }
+              classtype = classtype + classn;
+              return (
+                <div className="myapplication-row" key={index}>
+                  <p className="myapplication-row-text ">
+                    {application.rpdata.title}
                   </p>
-                  <p className="myapplication-row-text myapplication-row-text-head ">
-                    Application Date
+                  <p className="myapplication-row-text ">
+                    {application.applicationstatus.date.substring(0, 10)}
                   </p>
-                  <p className="myapplication-row-text myapplication-row-text-head ">
-                    Status
+                  <p className="myapplication-row-text  ">
+                    <button
+                      onClick={async () => {
+                        if (application.applicationstatus.data == 1) {
+                          setisLoading(true);
+                          console.log(
+                            application.applicationstatus.appication[0]
+                          );
+
+                          window.location = `/quiz/${application.applicationstatus.appication[0].researchProgram}/${application.applicationstatus.appication[0].position}/${application.applicationstatus.appication[0]._id}`;
+                        }
+                      }}
+                      className={classtype}
+                    >
+                      {title}
+                    </button>
+                    {}
                   </p>
                 </div>
-                {applicationsData.map((application, index) => {
-                  let classn = "",
-                    title = "",
-                    classtype = "myapplication-row-btn ";
-
-                  switch (application.applicationstatus.data) {
-                    case 7:{
-                      classn = "myapplication-row-btn-sub";
-                      title = "Submited";
-                      break;
-                    }
-                    case 6:{
-                      classn = "myapplication-row-btn-accept";
-                      title = "Archived";
-                      break;
-                    }
-                    case 5:{
-                      classn = "myapplication-row-btn-arch";
-                      title = "Accepted";
-                      break;
-                    }
-                    case 4: {
-                      classn = "myapplication-row-btn-approv";
-                      title = "Approved";
-                      break;
-                    }
-                    case 3: {
-                      classn = "myapplication-row-btn-rej";
-                      title = "Rejected";
-                      break;
-                    }
-                    case 2: {
-                      classn = "myapplication-row-btn-pend";
-                      title = "Under Review";
-                      break;
-                    }
-                    case 1: {
-                      classn = "myapplication-row-btn-sub";
-                      title = "Attend Quiz";
-                      break;
-                    }
-                  }
-                  classtype = classtype + classn;
-                  return (
-                    <div className="myapplication-row" key={index}>
-                      <p className="myapplication-row-text ">
-                        {application.rpdata.title}
-                      </p>
-                      <p className="myapplication-row-text ">
-                        {application.applicationstatus.date.substring(0, 10)}
-                      </p>
-                      <p className="myapplication-row-text  ">
-                        <button onClick={async()=>{
-                          if(application.applicationstatus.data==1)
-                          {setisLoading(true)
-                            console.log(application.applicationstatus.appication[0])
-                            
-                           window.location=`/quiz/${application.applicationstatus.appication[0].researchProgram}/${application.applicationstatus.appication[0].position}/${application.applicationstatus.appication[0]._id}`
-                          }
-                        }} className={classtype}>{title}</button>
-                        {}
-                      </p>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-            <div
-              className="holder-divv"
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                flexDirection: "column",
-              }}
-            >
-              <div className="applications-heading">
-                <h2>Selection Process</h2>
-              </div>
-              <img
-                src="/images/app-status.svg"
-                alt="app-status"
-                className="app-status app-status-home"
-              />
-            </div>
-            <Footer/>
+              );
+            })}
           </div>
-        )
-    
+        </div>
+        <div
+          className="holder-divv"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <div className="applications-heading">
+            <h2>Selection Process</h2>
+          </div>
+          <img
+            src="/images/app-status.svg"
+            alt="app-status"
+            className="app-status app-status-home"
+          />
+        </div>
+        <Footer />
+      </div>
+      )
     </>
   );
 };
