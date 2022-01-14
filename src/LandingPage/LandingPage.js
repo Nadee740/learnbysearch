0cc1d023440dd6cd9ed19573cd089d9eb9f9f5c7
dontech09 <donnjondanmakal@gmail.com>
2022-01-14 20:59:19 +0530

    Changes to home new section activities

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -293,7 +293,7 @@ const LandingPage = () => {
             </div>
           </section>
           <div className="slider-webinar">
-            <Slider {...settings}>
+            {/**<Slider {...settings}>
               {webinardata.map((webinar, index) => {
                 return webinar.isOpen ? (
                   <WebinarSlider
@@ -307,7 +307,7 @@ const LandingPage = () => {
                   ""
                 );
               })}
-            </Slider>
+            </Slider> */}
           </div>
           {/*<section className="second">
          
@@ -454,8 +454,7 @@ const LandingPage = () => {
               </div>
             </div>
           </section>
-          {/*<Testimonial testimonials={testimonialsdata} />*/}
-          <div
+          {/*<Testimonial testimonials={testimonialsdata} /> <div
             className="holder-divv"
             style={{
               display: "flex",
@@ -472,7 +471,7 @@ const LandingPage = () => {
               alt="app-status"
               className="app-status app-status-home"
             />
-          </div>
+          </div>*/}
 
           <div className="applydetails">
             <div className="applydetails-col1">
