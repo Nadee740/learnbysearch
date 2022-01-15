776d057254d48dbe013949ad6a9ee9c00265316d
dontech09 <donnjondanmakal@gmail.com>
2022-01-15 15:29:59 +0530

    Changes to home new section activities and webinar

--- src/LandingPage/banner/banner.js
+++ src/LandingPage/banner/banner.js
@@ -3,11 +3,7 @@ import "./banner.css";
 const BannerBtn = () => {
   return (
     <div className="bannerbtn">
-      <h3 className="bannerbtn-text">
-        We're your startegic learning partner.
-        <br />
-        Let's move skills forward together.
-      </h3>
+      <h3 className="bannerbtn-text">Upcoming Research programs</h3>
       <a href="/ongoingprogrammes">
         <button className="bannerbtn-btn">Apply Now</button>
       </a>
